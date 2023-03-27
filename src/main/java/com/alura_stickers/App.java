00ddc834b961dc506b6dd9660769582cfc409d48
André Dantas <43660680+andre-haniak@users.noreply.github.com>
2023-03-27 14:38:14 -0300

    listing all movies

--- src/main/java/com/alura_stickers/App.java
+++ src/main/java/com/alura_stickers/App.java
@@ -19,11 +19,18 @@ public class App {
         HttpRequest request = HttpRequest.newBuilder(address).GET().build();
         HttpResponse<String> response = httpClient.send(request, BodyHandlers.ofString());
         String body = response.body();
-        System.out.println(body);
         
         // Extracting data from the $body{title, image, rating}
         JsonParser parser = new JsonParser();
 
         List<Map<String, String>> movieList = parser.parse(body);
+
+
+        // Listing all movies
+        for (Map<String,String> movie : movieList) {
+            System.out.println(movie.get("title"));
+            System.out.println(movie.get("image"));
+            System.out.println(movie.get("imDbRating"));
+        }
     }
 }
