a43adf2ce3bfa19a82cad0bc9aff2c51b898b20d
André Dantas <43660680+andre-haniak@users.noreply.github.com>
2023-03-27 14:12:28 -0300

    created first request for top movies API

--- src/main/java/com/alura_stickers/App.java
+++ src/main/java/com/alura_stickers/App.java
@@ -1,13 +1,25 @@
 package com.alura_stickers;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // Making request to top movies API
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+
+        URI address = URI.create(url);
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(address).GET().build();
+        HttpResponse<String> response = httpClient.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        System.out.println(body);
+
     }
 }
