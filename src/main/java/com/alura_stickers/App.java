485a55f9bb8babce7227254ed7189f377275211c
André Dantas <43660680+andre-haniak@users.noreply.github.com>
2023-03-29 15:25:08 -0300

    performed refactoring by creating the content and httpclient class

--- src/main/java/com/alura_stickers/App.java
+++ src/main/java/com/alura_stickers/App.java
@@ -1,46 +1,38 @@
 package com.alura_stickers;
 
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
-        // Making request to top movies API
-        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        // Making request to top contents API
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/Topcontents.json";
 
-        URI address = URI.create(url);
-        HttpClient httpClient = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder(address).GET().build();
-        HttpResponse<String> response = httpClient.send(request, BodyHandlers.ofString());
-        String body = response.body();
+        ClientHttp http = new ClientHttp();
+        String json = http.searchData(url);
         
         // Extracting data from the $body{title, image, rating}
         JsonParser parser = new JsonParser();
+        List<Map<String, String>> contentList = parser.parse(json);
 
-        List<Map<String, String>> movieList = parser.parse(body);
-
-        // Listing all movies
+        // Listing all contents
         StickerGenerator generator = new StickerGenerator();
-        for (Map<String,String> movie : movieList) {
 
-            String title = movie.get("title");
-            String rating = movie.get("imDbRating");
-            String urlImage = movie.get("image");
+        for (Map<String,String> content : contentList) {
+
+            String title = content.get("title");
+            String rating = content.get("imDbRating");
+            String urlImage = content.get("image");
             String fileName = title.replace(":", "-")  + ".png";
 
             InputStream inputStream = new URL(urlImage).openStream();
             generator.CreateSticker(inputStream, fileName, rating);
 
 
-            // Print movie info with decorations
+            // Print content info with decorations
             System.out.println("\u001b[1m" + title + "\u001b[0m");
             System.out.println("Rating: " + Stars(rating) + " " + rating);
             System.out.println("\u001b[3m" + "-----------------------------" + "\u001b[0m");
---
+++ src/main/java/com/alura_stickers/ClientHttp.java
@@ -0,0 +1,28 @@
+package com.alura_stickers;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+public class ClientHttp {
+    
+    public String searchData(String url){
+        
+        try {
+            URI address = URI.create(url);
+            HttpClient httpClient = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder(address).GET().build();
+            HttpResponse<String> response = httpClient.send(request, BodyHandlers.ofString());
+            String body = response.body();
+
+            return body;
+        } catch (IOException | InterruptedException ex) {
+            throw new RuntimeException(ex);
+        }
+
+
+    }
+}
