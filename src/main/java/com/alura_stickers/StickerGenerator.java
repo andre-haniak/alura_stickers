6635bcadc331cf3397a5657cd4f944f344b20a82
André Dantas <43660680+andre-haniak@users.noreply.github.com>
2023-03-29 10:31:17 -0300

    writing sentence on new image

--- src/main/java/com/alura_stickers/StickerGenerator.java
+++ src/main/java/com/alura_stickers/StickerGenerator.java
@@ -1,5 +1,7 @@
 package com.alura_stickers;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -17,7 +19,7 @@ public class StickerGenerator {
         //Create new image in memory with transparency and new size
         int width = originalImage.getWidth();
         int height = originalImage.getHeight();
-        int newHeight = height + 300;
+        int newHeight = height + 200;
         BufferedImage newImage = new BufferedImage(width, newHeight, BufferedImage.TRANSLUCENT);
 
         //Copy image to new image
@@ -25,8 +27,14 @@ public class StickerGenerator {
 
         graphics.drawImage(originalImage, 0, 0, null);
 
-        //Write a sentence on the new image
+        //Configuring font
+        Font font = new Font(Font.SANS_SERIF, Font.BOLD, 64);
+        graphics.setColor(Color.PINK);
+        graphics.setFont(font);
 
+
+        //Write a sentence on the new image
+        graphics.drawString("Approved", 0, newHeight - 100);
 
         //Write new image to a file
         ImageIO.write(newImage, "png", new File("output/sticker.png"));
