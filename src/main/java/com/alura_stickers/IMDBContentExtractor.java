538e2333498fc180ace34f77b2936308cf0e1c1a
André Dantas <43660680+andre-haniak@users.noreply.github.com>
2023-03-30 12:11:47 -0300

    Streams implemented in extractor classes

--- src/main/java/com/alura_stickers/IMDBContentExtractor.java
+++ src/main/java/com/alura_stickers/IMDBContentExtractor.java
@@ -1,8 +1,8 @@
 package com.alura_stickers;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ImdbContentExtractor  implements ContentExtractor{
     
@@ -11,21 +11,8 @@ public class ImdbContentExtractor  implements ContentExtractor{
         JsonParser parser = new JsonParser();
         List<Map<String, String>> attributeList = parser.parse(json);
 
-        List<Content> contents = new ArrayList<>();
-        System.out.println(contents);
-        
-        //Popular content list
-        for (Map<String, String> attributes: attributeList) {
-            String title = attributes.get("title");
-            String urlImage = attributes.get("image");
-            // .replaceAll("(@+)(.*).jpg$", "$1.jpg");
-
-            Content content = new Content(title, urlImage);
-
-            contents.add(content);
-        }
-
-        return contents;
+        return attributeList.stream()
+            .map(Content::toContent)
+            .collect(Collectors.toList());
     } 
-
 }
--- src/main/java/com/alura_stickers/NasaContentExtractor.java
+++ src/main/java/com/alura_stickers/NasaContentExtractor.java
@@ -1,8 +1,8 @@
 package com.alura_stickers;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class NasaContentExtractor implements ContentExtractor {
 
@@ -12,19 +12,9 @@ public class NasaContentExtractor implements ContentExtractor {
         JsonParser parser = new JsonParser();
         List<Map<String, String>> attributeList = parser.parse(json);
 
-        List<Content> contents = new ArrayList<>();
-
-        //Popular content list
-        for (Map<String, String> attributes: attributeList) {
-            String title = attributes.get("title");
-            String urlImage = attributes.get("url");
-            Content content = new Content(title, urlImage);
-
-            contents.add(content);
-        }
-
-        return contents;
-
+        return attributeList.stream()
+            .map(Content::toContent)
+            .collect(Collectors.toList());
     }
 
 }
